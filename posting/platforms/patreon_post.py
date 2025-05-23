927c40b40b2d23c4e9a14140183513c4a4d6f551
Bruno <bruno@platek.sk>
2025-05-23 17:26:28 +0200

    changing classes to xpath, got blocked. finish later

--- posting/platforms/patreon_post.py
+++ posting/platforms/patreon_post.py
@@ -56,40 +56,41 @@ def test_patreon_login(email, password, messages, cookie_reset):
         #print(driver.page_source, file=open("test.html", "w", encoding="utf-8"))
         if cookie_reset:
             sleep(5)
-        email_input_div = driver.find_element(By.CLASS_NAME, "sc-13e8657f-9.dLYKLV")
-        input_field = email_input_div.find_element(By.TAG_NAME, "input")
-        input_field.send_keys(email)
+        input_fields = driver.find_elements(By.TAG_NAME, "input")
+        email_field = [i for i in input_fields if i.accessible_name == "Email"][0]
+        email_field.send_keys(email)
         err = "Invalid email"
 
-        continue_button = driver.find_element(By.CLASS_NAME, "cm-dMgEsi.cm-LNraKM.cm-bcdZOb.cm-bephWK.cm-lCfmZu.cm-DFAJDB.cm-zbhPFN.cm-ooBxfa.cm-dupTbP.cm-UsNHpA.cm-TOsLDU")
+        continue_button = driver.find_element(By.XPATH, "//button[@type='submit']")
         continue_button.click()
         sleep(1)
 
-        password_input = driver.find_elements(By.CLASS_NAME, "sc-13e8657f-2.iMwXpw")[1]
+        input_fields = driver.find_elements(By.TAG_NAME, "input")
+        password_input = [i for i in input_fields if i.accessible_name == "Password"][0]
         password_input.send_keys(password)
 
         continue_button.click()
         err = "Incorrect password for this email. (if you're sure you typed it correctly, contact Bruno)"
         sleep(3)
 
-        member_button = driver.find_element(By.CLASS_NAME, "sc-a5153103-2.kTGxwx")
+        member_button = driver.find_element(By.XPATH, "//button[@data-tag='account-menu-toggle-switcher']")
         member_button.click() 
         err = "Internal Error (couldn't find the creator button). Contact Bruno"
         sleep(1)
 
-        creator_button = driver.find_element(By.CLASS_NAME, "sc-3888ea22-0.bHlqDk.cm-TsFebD")
+        creator_button = driver.find_element(By.XPATH, "//a[@aria-label='Go to creator home page']")
         creator_button.click()
         sleep(3)
 
-        create_button = driver.find_element(By.XPATH, "/html/body/div[1]/div[5]/main/div[1]/div/div/div/div[1]/div[2]/div/div/button")
+        create_button = driver.find_element(By.XPATH, "//button[@aria-label='Create post']")
         create_button.click()
-
-        post_choice = driver.find_element(By.XPATH, "/html/body/div[3]/div/div/div/div[2]/div/div/div/div/div/ul/li[1]/a")
+        
+        post_choice = driver.find_elements(By.XPATH, "//li[.//text()[contains(., 'Post')]]")[0]
         post_choice.click()
         sleep(3)
         
         err = "Internal Error (couldn't find title input field). Contact Bruno"
-        title_input = driver.find_element(By.CLASS_NAME, "sc-b73158da-0.kYunmr")
+        title_input = driver.find_elements(By.TAG_NAME, "textarea")[0]
     except:
         messages.append(err)
         driver.quit()
@@ -125,13 +126,13 @@ def patreon_post(title, text, cookie_reset):
         if driver == None:
             return messages
         sleep(4)
-        title_input = driver.find_element(By.CLASS_NAME, "sc-b73158da-0.kYunmr")
+        title_input = driver.find_elements(By.TAG_NAME, "textarea")[0]
         title_input.send_keys(title)
 
         # Transform the text before sending it
         transformed_text = transform_links(text)
-        
-        text_input = driver.find_element(By.CLASS_NAME, "ProseMirror.remirror-editor")
+
+        text_input = driver.find_elements(By.XPATH, "//div[@role='textbox']")
         # Split the text by newlines and send each part with a RETURN key
         for line in transformed_text.split('\n'):
             text_input.send_keys(line)
@@ -143,11 +144,13 @@ def patreon_post(title, text, cookie_reset):
             driver.quit()
             return messages
         
-        next_button = driver.find_element(By.CLASS_NAME, "cm-dMgEsi.cm-LNraKM.cm-bcdZOb.cm-bephWK.cm-lCfmZu.cm-DFAJDB.cm-zbhPFN.cm-dupTbP.cm-UsNHpA.cm-TOsLDU")
-        next_button.click()
-        sleep(2)
+        next_button = driver.find_elements(By.XPATH, "//button[.//text()[contains(., 'Next')]]")
+        print(len(next_button))
+        print(b.text for b in next_button)
+        next_button[0].click()
+        sleep(1)
 
-        publish = driver.find_element(By.CLASS_NAME, "cm-dMgEsi.cm-LNraKM.cm-bcdZOb.cm-bephWK.cm-lCfmZu.cm-DFAJDB.cm-zbhPFN.cm-dupTbP.cm-UsNHpA.cm-TOsLDU")
+        publish = driver.find_element(By.XPATH, "//button[.//text()[contains(., 'Publish')]]")
         publish.click()
         messages.append("Post successfully published!")
         driver.quit()
@@ -156,3 +159,7 @@ def patreon_post(title, text, cookie_reset):
         driver.quit()
 
     return messages
+
+if __name__ == "__main__":
+    msg = patreon_post("test", "tests", True)
+    print(msg)
